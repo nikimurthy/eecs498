2add401c4a18de9a64bf8c69806536de9c69b032
Ubuntu <ubuntu@ip-172-31-5-255.us-east-2.compute.internal>
2026-02-11 17:59:22 +0000

    chatter back end

--- chatterd/handlers.go
+++ chatterd/handlers.go
@@ -5,9 +5,26 @@ import (
     "net/http"
     "net/http/httputil"
     "net/url"
-
+    "time"
     "github.com/labstack/echo/v4"
 )
+
+type Chatt struct {
+    Name  string    `json:"name"`
+    Message   string    `json:"message"`
+    Id        string    `json:"id"`
+    Timestamp time.Time `json:"timestamp"`
+}
+
+func logServerErr(c echo.Context, err error) error {
+	log.Println("[Echo] |", http.StatusInternalServerError, `|`, c.RealIP(), `|`, c.Request().Method, c.Request().RequestURI, err.Error())
+	return c.JSON(http.StatusInternalServerError, err.Error())
+}
+
+func logClientErr(c echo.Context, sc int, err error) error {
+	log.Println("[Echo] |", sc, `|`, c.RealIP(), `|`, c.Request().Method, c.Request().RequestURI, err.Error())
+	return c.JSON(sc, err.Error())
+}
 
 func logOk(c echo.Context) {
 	log.Println("[Echo] |", http.StatusOK, `|`, c.RealIP(), `|`, c.Request().Method, c.Request().RequestURI)
@@ -29,5 +46,44 @@ func llmprompt(c echo.Context) error {
     proxy.ServeHTTP(c.Response(), req)
     logOk(c)
     return nil
+}
+
+func getchatts(c echo.Context) error {
+    var chattArr = [][]any{}
+    var chatt Chatt
+
+    rows, err := chatterDB.Query(background, `SELECT name, message, id, time FROM chatts ORDER BY time ASC`)
+    if err != nil {
+      if rows != nil { rows.Close() }
+		  return logServerErr(c, err)
+    }
+
+    for rows.Next() {
+        err = rows.Scan(&chatt.Name, &chatt.Message, &chatt.Id, &chatt.Timestamp)
+        if err != nil {
+          rows.Close()
+          return logServerErr(c, err)
+        }
+        chattArr = append(chattArr, []any{chatt.Name, chatt.Message, chatt.Id, chatt.Timestamp})
+    }
+
+    logOk(c)
+    return c.JSON(http.StatusOK, chattArr)
+}
+
+func postchatt(c echo.Context) error {
+    var chatt Chatt
+
+    if err := c.Bind(&chatt); err != nil {
+      return logClientErr(c, http.StatusUnprocessableEntity, err)
+    }
+
+    _, err := chatterDB.Exec(background, `INSERT INTO chatts (name, message, id) VALUES ($1, $2, gen_random_uuid())`, chatt.Name, chatt.Message)
+    if err != nil {
+        return logClientErr(c, http.StatusBadRequest, err)
+    }
+
+    logOk(c)
+	  return c.JSON(http.StatusOK, struct{}{}) // empty struct instance serialized to empty JSON: {}
 }
 
--- chatterd/main.go
+++ chatterd/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"log"
-  
+        "context"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -16,9 +17,17 @@ type Route struct {
 var routes = []Route {
 		// {"GET", "/", top},	
 		{"POST", "/llmprompt/", llmprompt},
+	        {"GET", "/getchatts/", getchatts},
+                {"POST", "/postchatt/", postchatt},
 }
 
+var background = context.Background()
+var chatterDB *pgxpool.Pool
+
 func main() {
+	var err error
+	chatterDB, err = pgxpool.Connect(background, "host=localhost user=chatter password=chattchatt dbname=chatterdb")
+	if err != nil { panic(err) }
 	server := echo.New()
 	server.HideBanner = true
 	for _, route := range routes {
@@ -29,5 +38,7 @@ func main() {
 	log.Fatal(server.StartTLS(":443",
 		"/home/ubuntu/reactive/chatterd.crt",
 		"/home/ubuntu/reactive/chatterd.key"))
+
+	
 }
 
